3cd54d5b165c70d1a10785510c80e26a60fc6378
Sohyeon Kim <sooooooohyeon5@naver.com>
2024-02-17 16:58:38 +0900

    [level 3] Title: 네트워크, Time: 0.21 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,6 +1,9 @@
-# 1 1 0
-# 1 1 0
-# 0 0 1
+# n개의 컴퓨터, map computers
+
+# 0 1 2 3
+# 1 1 1 0
+# 2 1 1 0
+# 3 0 0 1
 
 def dfs(computers, visited, v):
     visited[v] = True
@@ -8,15 +11,15 @@ def dfs(computers, visited, v):
     for i in range(len(computers)):
         if computers[v][i] == 1 and not visited[i]:
             dfs(computers, visited, i)
-    
+
 def solution(n, computers):
-    
+    visited = [False] * n
     answer = 0
-    visited = [False] * (n)
     
+    # 큰 배열
     for i in range(n):
-        if not visited[i]:
+        if visited[i] == False:
             dfs(computers, visited, i)
             answer += 1
-    
+            
     return answer
